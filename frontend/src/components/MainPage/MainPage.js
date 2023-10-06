22634b5f998d13f29533ef77e95e79d51a09abee
farivar <farivaramiri@gmail.com>
2023-10-05 22:04:08 -0700

    building info

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -4,15 +4,20 @@ import bg from '../../assets/fed-day.jpg';
 
 function MainPage() {
     const overlayRef = useRef(null);
+    const infoRef = useRef(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const offset = window.scrollY;
-            const opacity = Math.min(offset / 200, 3);
-            if (overlayRef.current) {
-                overlayRef.current.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-            }
-        };
+
+    useEffect(() => {const handleScroll = () => {
+      const offset = window.scrollY;
+      const opacity = Math.min(offset / 200, 1); // Cap at 1 for 100% opacity
+      if (overlayRef.current) {
+          overlayRef.current.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+      }
+      if (infoRef.current) {
+          infoRef.current.style.opacity = opacity;
+      }
+  };
+  
 
         window.addEventListener('scroll', handleScroll);
         return () => {
@@ -25,7 +30,10 @@ function MainPage() {
         <div className="main-page">
             <div className='bg-main'>
                 <img src={bg} alt="background picture (fed building)" />
-                <div className="bg-overlay" ref={overlayRef}></div> {/* Add this overlay div */}
+                <div className="bg-overlay" ref={overlayRef}></div>
+                <div>
+                  <h1 className='info-mainbg' ref={infoRef}>the Marriner S. Eccles Federal Reserve Board Building</h1>
+                </div>
             </div>
             <footer className="footer-m">&copy; 2023 F.A Financial Department</footer>
         </div>
