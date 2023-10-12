182c130b4cd527f1d06a266cfe39241e6afcd66c
farivar <farivaramiri@gmail.com>
2023-10-12 16:51:59 -0700

    ta first touch

--- frontend/src/components/Lessons/TechnicalAnalysis/TechnicalAnalysis.jsx
+++ frontend/src/components/Lessons/TechnicalAnalysis/TechnicalAnalysis.jsx
@@ -2,128 +2,74 @@ import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './TechnicalAnalysis.css';
-// import m1 from '../../../assets/m1.png';
-// import m2 from '../../../assets/m2.png';
-// import t1 from "../../../assets/t1.png";
-// import t2 from "../../../assets/t2.png";
-// import man from "../../../assets/man.png";
-// import plants from "../../../assets/plants.png";
+// You can import your assets here
+// For example:
+// import chart from '../../../assets/chart.png';
+// import indicators from '../../../assets/indicators.png';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const TechnicalAnalysis = () => {
     useEffect(() => {
-        gsap.from("#forex-m1", {
-            scrollTrigger: {
-                scrub: true
-            },
-            y: 100,
-        });
-        gsap.from("#forex-m2", {
-            scrollTrigger: {
-                scrub: true
-            },
-            y: 50,
-        });
-        gsap.from("#forex-t2", {
-            scrollTrigger: {
-                scrub: true
-            },
-            x: -50,
-        });
-        gsap.from("#forex-t1", {
-            scrollTrigger: {
-                scrub: true
-            },
-            x: 50,
-        });
-        gsap.from("#forex-man", {
-            scrollTrigger: {
-                scrub: true
-            },
-            x: -250,
-        });
-        gsap.from("#forex-plants", {
-            scrollTrigger: {
-                scrub: true
-            },
-            x: -50,
-        });
-        gsap.from("#forex-text", {
-            scrollTrigger: {
-                scrub: true
-            },
-            x: 600,
-        });
+        // Add your gsap animations here
+        // For example:
+        // gsap.from("#chart-types", { ... });
+        // gsap.from("#indicators", { ... });
     }, []);
+
     return (
-        <div className="introduction-to-forex-page">
-            {/* <section className="forex-parallax">
-                <h2 id="forex-text">FOREX</h2>
-                <img src={m1} id="forex-m1" alt="Mountain 1"/>
-                <img src={t2} id="forex-t2" alt="Trees 2"/>
-                <img src={m2} id="forex-m2" alt="Mountain 2"/>
-                <img src={t1} id="forex-t1" alt="Trees 1"/>
-                <img src={man} id="forex-man" alt="Man"/>
-                <img src={plants} id="forex-plants" alt="Plants"/>
-            </section> */}
-            <section className="forex-sec">
-                <h2>TechnicalAnalysis</h2>
-                <p>Forex (FX) is a portmanteau of the words foreign [currency] and exchange. Foreign exchange is the process of changing one currency into another for various reasons, usually for commerce, trading, or tourism. According to a 2022 triennial report from the Bank for International Settlements (a global bank for national central banks), the daily global volume for forex trading reached $7.5 trillion in 2022.</p>
-                <br />
+        <div className="technical-analysis-page">
+            <section className="tech-analysis-parallax">
+                {/* Add your images and elements here */}
+                {/* For example:
+                <img src={chart} id="chart-types" alt="Chart Types"/>
+                <img src={indicators} id="indicators" alt="Indicators"/>
+                */}
+            </section>
+            <section className="tech-analysis-sec">
+                <h2>Technical Analysis</h2>
+                <p>Technical analysis involves predicting future price movements based on past market data. Traders use various tools and techniques, including charts and indicators, to analyze and interpret market data.</p>
                 <br />
                 <h3>
-                    Read on to learn about the forex markets, what they're used for, and how to start trading.
+                    Explore the tools and techniques used in technical analysis below.
                 </h3>
-                
             </section>
-            <div className="itf-container">
-    <div className="itf-box" style={{ "--clr": "#89ec5b" }}>
-        <div className="itf-content">
-            <div className="itf-icon"><ion-icon name="search"></ion-icon></div>
-            <div className="itf-text">
-                <h3>1. What is Forex?</h3>
-                <p>The foreign exchange market – also known as forex or FX – is the world’s most traded market.</p>
-                <a href="/Lessons/IntroductionToForex/WhatIsForex">Read More</a>
-            </div>
-        </div>
-    </div>
-    <div className="itf-box" style={{ "--clr": "#00ffe1" }}>
-    <div class="itf-content">
-                <div class="itf-icon"><ion-icon name="logo-usd"></ion-icon></div>
-                <div class="itf-text">
-                    <h3>2. Major Currency Pairs</h3>
-                    <p>The major currency pairs are some of the most popular currency combinations in the forex market. Prices in these pairs often move in tighter bands, but their movements can still be volatile</p>
-                    <a href="/Lessons/IntroductionToForex/MajorCurrencyPairs">Read More</a>
+            <div className="ta-container">
+                <div className="ta-box" style={{ "--clr": "#f39c12" }}>
+                    <div className="ta-content">
+                        <div className="ta-icon"><ion-icon name="bar-chart-outline"></ion-icon></div>
+                        <div className="ta-text">
+                            <h3>1. Chart Types</h3>
+                            <p>Learn about different chart types like line, bar, and candlestick charts and how they are used in technical analysis.</p>
+                            <a href="/Lessons/TechnicalAnalysis/ChartTypes">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="ta-box" style={{ "--clr": "#e74c3c" }}>
+                    <div className="ta-content">
+                        <div className="ta-icon"><ion-icon name="pulse-outline"></ion-icon></div>
+                        <div className="ta-text">
+                            <h3>2. Indicators</h3>
+                            <p>Discover various technical indicators like Moving Averages, MACD, RSI, and more. Understand their significance and application in trading.</p>
+                            <a href="/Lessons/TechnicalAnalysis/Indicators">Read More</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-    </div>
-    <div className="itf-box" style={{ "--clr": "#5b98eb" }}>
-    <div class="itf-content">
-                <div class="itf-icon"><ion-icon name="time-outline"></ion-icon></div>
-                <div class="itf-text">
-                    <h3>3. Forex Market Hours</h3>
-                    <p>The forex market is open 24 hours a day during the weekdays which allows traders to potentially trade all day and all night.</p>
-                    <a href="/Lessons/IntroductionToForex/ForexMarketHours">Read More</a>
-                </div>
-            </div>
-            </div>
-        </div>
 
-        <div className="itf-container">
-            <div className='back-to-int'>
-                <a href="/Lessons">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Back to Lessons
-                </a>
+            <div className="ta-container">
+                <div className='back-to-int'>
+                    <a href="/Lessons">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Back to Lessons
+                    </a>
+                </div>
             </div>
-        </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/gsap.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/ScrollTrigger.min.js"></script>
-    
         </div>
     );
 }
