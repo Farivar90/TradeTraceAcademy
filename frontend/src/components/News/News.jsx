0bfb591e926d65c4460cde4f3a4ad9329804263c
farivar <farivaramiri@gmail.com>
2023-10-15 16:00:51 -0700

    news animation

--- frontend/src/components/News/News.jsx
+++ frontend/src/components/News/News.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import NewsBg from '../../assets/news1.gif';
+import React, { useEffect }  from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import NewsBg from '../../assets/news1.gif';
+import NewsBg from '../../assets/news-table.webp';
+import NewsW1 from '../../assets/newswoman.png';
+import NewsW2 from '../../assets/newswoman2.png';
 import './News.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const News = () => {
+useEffect(() => {
+    gsap.from("#forex-m1", {
+        scrollTrigger: {
+            scrub: true
+        },
+        y: 100,
+    });
+    gsap.from("#forex-m2", {
+        scrollTrigger: {
+            scrub: true
+        },
+        y: 50,
+    });
+    gsap.from("#forex-t2", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -50,
+    });
+    gsap.from("#forex-t1", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: 50,
+    });
+    gsap.from("#forex-man", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -250,
+    });
+    gsap.from("#forex-plants", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -50,
+    });
+    gsap.from("#news-text", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: 600,
+    });
+}, []);
     return (
         <div className='news-main'>
-                <div className="imageContainer">
-                {/* <img className='news-bg' src={NewsBg} alt="newsbg" /> */}
-            </div>
+            <section className="news-parallax">
+                <h2 id="news-text">NEWS</h2>
+                <img id='news-table' src={NewsBg} alt="news table" />
+                <img id='news-woman' src={NewsW1} alt="news woman" />
+                <img id='news-woman2' src={NewsW2} alt="news woman2" />
+            </section>
+                
+           
         </div>
     );
 };
