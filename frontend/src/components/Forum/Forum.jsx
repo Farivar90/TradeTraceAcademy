2fea11086d93e9a5205cd31f3a528c7888a23625
farivar <farivaramiri@gmail.com>
2023-10-16 22:29:27 -0700

    ready for hosting

--- frontend/src/components/Forum/Forum.jsx
+++ frontend/src/components/Forum/Forum.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import './Forum.css';
+import AnimatedSVG from  '../Loading/ALoader';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Forum = () => {
-    return (
-        <div>
 
+
+    return (
+        <div className="introduction-to-forex-page">
+            {/* <section className="forex-parallax">
+                <h2 id="forex-text">FOREX</h2>
+                <img src={m1} id="forex-m1" alt="Mountain 1"/>
+                <img src={t2} id="forex-t2" alt="Trees 2"/>
+                <img src={m2} id="forex-m2" alt="Mountain 2"/>
+                <img src={t1} id="forex-t1" alt="Trees 1"/>
+                <img src={man} id="forex-man" alt="Man"/>
+                <img src={plants} id="forex-plants" alt="Plants"/>
+            </section> */}
+            <section className="forex-sec">
+                <h2>Under Construction</h2>
+               
+
+                
+            </section>
+            <AnimatedSVG/>
+
+        <div className="itf-container">
+            <div className='back-to-int'>
+                <a href="/">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Back to Main Page
+                </a>
+            </div>
+        </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/gsap.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/ScrollTrigger.min.js"></script>
+    
         </div>
     );
-};
+}
 
 export default Forum;
--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,17 +1,49 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-// import { fetchUserTweets, clearTweetErrors } from '../../store/tweets';
-// import TweetBox from '../Tweets/TweetBox';
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './Profile.css';
+import AnimatedSVG from  '../Loading/ALoader';
 
-function Profile () {
+gsap.registerPlugin(ScrollTrigger);
+
+const Profile = () => {
+
 
     return (
-      <div className="profile">
-    
-    </div>
-  );
-}
+        <div className="introduction-to-forex-page">
+            {/* <section className="forex-parallax">
+                <h2 id="forex-text">FOREX</h2>
+                <img src={m1} id="forex-m1" alt="Mountain 1"/>
+                <img src={t2} id="forex-t2" alt="Trees 2"/>
+                <img src={m2} id="forex-m2" alt="Mountain 2"/>
+                <img src={t1} id="forex-t1" alt="Trees 1"/>
+                <img src={man} id="forex-man" alt="Man"/>
+                <img src={plants} id="forex-plants" alt="Plants"/>
+            </section> */}
+            <section className="forex-sec">
+                <h2>Under Construction</h2>
+               
 
+                
+            </section>
+            <AnimatedSVG/>
+
+        <div className="itf-container">
+            <div className='back-to-int'>
+                <a href="/">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Back to Main Page
+                </a>
+            </div>
+        </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/gsap.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/ScrollTrigger.min.js"></script>
+    
+        </div>
+    );
+}
 
 export default Profile;
