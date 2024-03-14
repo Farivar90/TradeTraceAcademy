c2ad371de29d9026548d3c64a25f74d4ba58efa8
farivar <farivaramiri@gmail.com>
2024-03-13 23:19:02 -0700

    share!

--- frontend/src/components/SocialShare/SocialShare.jsx
+++ frontend/src/components/SocialShare/SocialShare.jsx
@@ -7,34 +7,32 @@ const SocialShare = () => {
         menu.classList.toggle("active");
     };
 
+    const currentPageUrl = encodeURIComponent(window.location.href);
+    const emailSubject = encodeURIComponent("Check this out!");
+    const emailBody = encodeURIComponent("I thought you might find this interesting: " + window.location.href);
+
     return (
         <div className="social-share">
             <div className="toggle" onClick={toggleMenu}>
                 <ion-icon name="share-social"></ion-icon>
             </div>
             <li style={{ '--i': 0, '--clr': '#1877f2' }}>
-                <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                <a href={`https://www.facebook.com/sharer/sharer.php?u=${currentPageUrl}`}><ion-icon name="logo-facebook"></ion-icon></a>
             </li>
             <li style={{ '--i': 1, '--clr': '#25d366' }}>
-                <a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a>
+                <a href={`https://wa.me/?text=${currentPageUrl}`}><ion-icon name="logo-whatsapp"></ion-icon></a>
             </li>
             <li style={{ '--i': 2, '--clr': '#1da1f2' }}>
-                <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                <a href={`https://twitter.com/intent/tweet?url=${currentPageUrl}&text=Check this out!`}><ion-icon name="logo-twitter"></ion-icon></a>
             </li>
             <li style={{ '--i': 3, '--clr': '#FF5733' }}>
-                <a href="#"><ion-icon name="logo-reddit"></ion-icon></a>
+                <a href={`https://www.reddit.com/submit?url=${currentPageUrl}&title=Interesting%20Page`}><ion-icon name="logo-reddit"></ion-icon></a>
             </li>
             <li style={{ '--i': 4, '--clr': '#0a66c2' }}>
-                <a href="#"><ion-icon name="logo-linkedin"></ion-icon></a>
+                <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentPageUrl}`}><ion-icon name="logo-linkedin"></ion-icon></a>
             </li>
-            <li style={{ '--i': 5, '--clr': '#c32aa3' }}>
-                <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
-            </li>
-            {/* <li style={{ '--i': 6, '--clr': '#1b1e21' }}>
-                <a href="#"><ion-icon name="logo-github"></ion-icon></a>
-            </li> */}
             <li style={{ '--i': 7, '--clr': '#0a66c2' }}>
-                <a href="#"><ion-icon name="paper-plane"></ion-icon></a>
+                <a href={`mailto:?subject=${emailSubject}&body=${emailBody}`}><ion-icon name="paper-plane"></ion-icon></a>
             </li>
         </div>
     );
