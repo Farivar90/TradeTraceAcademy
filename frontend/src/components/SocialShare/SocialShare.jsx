30b3d10ce798b575a5cd31acd74ffa64e178f1c5
farivar <farivaramiri@gmail.com>
2024-03-13 23:25:51 -0700

    share!

--- frontend/src/components/SocialShare/SocialShare.jsx
+++ frontend/src/components/SocialShare/SocialShare.jsx
@@ -17,22 +17,22 @@ const SocialShare = () => {
                 <ion-icon name="share-social"></ion-icon>
             </div>
             <li style={{ '--i': 0, '--clr': '#1877f2' }}>
-                <a href={`https://www.facebook.com/sharer/sharer.php?u=${currentPageUrl}`}><ion-icon name="logo-facebook"></ion-icon></a>
+                <a href={`https://www.facebook.com/sharer/sharer.php?u=${currentPageUrl}`}><ion-icon name="Facebook"></ion-icon></a>
             </li>
             <li style={{ '--i': 1, '--clr': '#25d366' }}>
-                <a href={`https://wa.me/?text=${currentPageUrl}`}><ion-icon name="logo-whatsapp"></ion-icon></a>
+                <a href={`https://wa.me/?text=${currentPageUrl}`}><ion-icon name="Whatsapp"></ion-icon></a>
             </li>
             <li style={{ '--i': 2, '--clr': '#1da1f2' }}>
-                <a href={`https://twitter.com/intent/tweet?url=${currentPageUrl}&text=Check this out!`}><ion-icon name="logo-twitter"></ion-icon></a>
+                <a href={`https://twitter.com/intent/tweet?url=${currentPageUrl}&text=Check this out!`}><ion-icon name="Twitter"></ion-icon></a>
             </li>
             <li style={{ '--i': 3, '--clr': '#FF5733' }}>
-                <a href={`https://www.reddit.com/submit?url=${currentPageUrl}&title=Interesting%20Page`}><ion-icon name="logo-reddit"></ion-icon></a>
+                <a href={`https://www.reddit.com/submit?url=${currentPageUrl}&title=Interesting%20Page`}><ion-icon name="Reddit"></ion-icon></a>
             </li>
             <li style={{ '--i': 4, '--clr': '#0a66c2' }}>
-                <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentPageUrl}`}><ion-icon name="logo-linkedin"></ion-icon></a>
+                <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentPageUrl}`}><ion-icon name="Linkedin"></ion-icon></a>
             </li>
             <li style={{ '--i': 7, '--clr': '#0a66c2' }}>
-                <a href={`mailto:?subject=${emailSubject}&body=${emailBody}`}><ion-icon name="paper-plane"></ion-icon></a>
+                <a href={`mailto:?subject=${emailSubject}&body=${emailBody}`}><ion-icon name="Email"></ion-icon></a>
             </li>
         </div>
     );
